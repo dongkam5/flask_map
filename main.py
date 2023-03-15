d85a331f654935d3f9fbb21f992fb00c5a047553
dongkam <84716824+dongkam5@users.noreply.github.com>
2023-03-16 00:00:34 +0900

    complete

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ def select_all():
         db.close()
         return ret
 
+@app.before_request
+def before_request():
+    scheme = request.headers.get('X-Forwarded-Proto')
+    if scheme and scheme == 'http' and request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
+
 @app.route('/')
 def index():
     stores=select_all()
