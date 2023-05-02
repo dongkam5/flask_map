02a75db607b5ba7d2d1e5527420a1d15b0e6186e
dongkam <84716824+dongkam5@users.noreply.github.com>
2023-05-03 00:15:55 +0900

    add screenshot

--- main.py
+++ main.py
@@ -18,14 +18,7 @@ def select_all():
     finally:
         db.close()
         return ret
-
-@app.before_request
-def before_request():
-    scheme = request.headers.get('X-Forwarded-Proto')
-    if scheme and scheme == 'http' and request.url.startswith('http://'):
-        url = request.url.replace('http://', 'https://', 1)
-        code = 301
-        return redirect(url, code=code)
+    
 
 @app.route('/')
 def index():
